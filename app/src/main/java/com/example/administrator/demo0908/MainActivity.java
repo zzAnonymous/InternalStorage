29140b7d001a21c1415b9cfdbc099b31a6729eb3
zzAnonymous <zyt_bob@163.com>
2017-09-12 08:26:31 +0800

    Initial commit

---
+++ app/src/main/java/com/example/administrator/demo0908/MainActivity.java
@@ -0,0 +1,85 @@
+package com.example.administrator.demo0908;
+
+import android.os.Build;
+import android.support.annotation.RequiresApi;
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
+
+import java.io.BufferedInputStream;
+import java.io.BufferedOutputStream;
+import java.io.FileInputStream;
+import java.io.FileOutputStream;
+import java.io.InputStream;
+import java.io.OutputStream;
+import java.nio.charset.StandardCharsets;
+
+public class MainActivity extends AppCompatActivity {
+
+    private final static String MyFileName="myfile";
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        Button btwrite = (Button) findViewById(R.id.btwrite);
+        Button btread= (Button) findViewById(R.id.btread);
+        btwrite.setOnClickListener(new View.OnClickListener() {
+            @RequiresApi(api = Build.VERSION_CODES.KITKAT)
+            @Override
+            public void onClick(View v) {
+                OutputStream out=null;
+                try {
+                    FileOutputStream fileOutputStream=openFileOutput(MyFileName,MODE_PRIVATE);
+                    out=new BufferedOutputStream(fileOutputStream);
+                    EditText editText = (EditText) findViewById(R.id.str);
+                    String content = editText.getText().toString();
+                    try {
+                        out.write(content.getBytes(StandardCharsets.UTF_8));
+                    }
+                    finally {
+                        if(out!=null)
+                            out.close();
+                    }
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+        btread.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                InputStream in=null;
+                try {
+                    FileInputStream fileInputStream = openFileInput(MyFileName);
+                    in=new BufferedInputStream(fileInputStream);
+
+                    int c;
+                    StringBuilder stringBuilder=new StringBuilder("");
+                    try{
+                        while ((c=in.read())!=-1) {
+                            stringBuilder.append((char)c);
+                        }
+                        Toast.makeText(MainActivity.this,stringBuilder.toString(), Toast.LENGTH_LONG).show();
+                    }
+                    finally {
+                        if(in!=null)
+                            in.close();
+                    }
+                }
+                catch (Exception e){
+                    e.printStackTrace();
+                }
+
+            }
+        });
+
+    }
+}
